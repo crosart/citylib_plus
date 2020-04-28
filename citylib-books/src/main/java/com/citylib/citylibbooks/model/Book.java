b2edb1e3ce947ba3ef5a69b96877440c16f1007d
CROS Arthur <cros.arthur@gmail.com>
2020-04-28 10:25:11 +0200

    Cleaning Lombok annotations
    Moving @Getters and @Setters to the Class instead of each variable (as all variables need getters & setters

--- citylib-books/src/main/java/com/citylib/citylibbooks/model/Book.java
+++ citylib-books/src/main/java/com/citylib/citylibbooks/model/Book.java
@@ -10,34 +10,23 @@ import javax.persistence.*;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Table(schema = "citylib_books", name = "books")
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter @Setter
     private long id;
 
-    @Getter @Setter
     private String isbn;
-
-    @Getter @Setter
     private String title;
-
-    @Getter @Setter
     private String author;
-
-    @Getter @Setter
     private String editor;
-
-    @Getter @Setter
     private String collection;
-
-    @Getter @Setter
     private int year;
 
     @JsonIgnore
-    @Getter @Setter
     private int availability;
 
 }
