d962b1100a71b710b79ccc5eeda65188a6595e60
CROS Arthur <cros.arthur@gmail.com>
2020-04-30 08:54:06 +0200

    Creating the Batch Service
    Adding the Batch Service.

---
+++ citylib-batch/src/main/java/com/citylib/citylibbatch/CitylibBatchApplication.java
@@ -0,0 +1,13 @@
+package com.citylib.citylibbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitylibBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitylibBatchApplication.class, args);
+	}
+
+}
