298cedf5983a28c0e5c6f10e7798ae2b18c27ead
CROS Arthur <cros.arthur@gmail.com>
2020-05-20 10:41:29 +0200

    Adding a dedicated User controller

---
+++ citylib-webapp/src/main/java/com/citylib/citylibwebapp/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.citylib.citylibwebapp.controller;
+
+import com.citylib.citylibwebapp.model.BookBean;
+import com.citylib.citylibwebapp.model.LoanBean;
+import com.citylib.citylibwebapp.model.UserBean;
+import com.citylib.citylibwebapp.proxy.CitylibServicesProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private CitylibServicesProxy servicesProxy;
+
+    @GetMapping("/account")
+    public String showAccountPage(Model model, Principal principal) {
+        UserBean loggedUser = servicesProxy.getUserByEmail(principal.getName());
+        List<LoanBean> userLoans = servicesProxy.getUserLoans(loggedUser.getId());
+        for (LoanBean loanBean : userLoans) {
+            if (loanBean.getDue().isBefore(LocalDate.now())) {
+                loanBean.setExpired(true);
+            } else {
+                loanBean.setExpired(false);
+            }
+        }
+        model.addAttribute("loggedUser", loggedUser);
+        model.addAttribute("userLoans", userLoans);
+        return "my_account";
+    }
+
+}
