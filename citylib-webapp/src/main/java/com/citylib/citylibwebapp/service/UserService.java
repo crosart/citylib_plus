40728613557907185556f6d8e99f61a8f52e9e8c
CROS Arthur <cros.arthur@gmail.com>
2020-05-20 10:43:56 +0200

    Creating the UserService and Implementations
    Authorization and Registration to complete Security.

--- citylib-webapp/src/main/java/com/citylib/citylibwebapp/service/UserService.java
+++ citylib-webapp/src/main/java/com/citylib/citylibwebapp/service/UserService.java
@@ -1,16 +1,12 @@
 package com.citylib.citylibwebapp.service;
 
 import com.citylib.citylibwebapp.dto.UserDto;
-import com.citylib.citylibwebapp.model.User;
-import org.springframework.cloud.openfeign.FeignClient;
+import com.citylib.citylibwebapp.model.UserBean;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 public interface UserService extends UserDetailsService {
 
-    User findByEmail(String email);
-    User save(UserDto registration);
+    UserBean findByEmail(String email);
+    UserBean save(UserDto registration);
 
 }
--- citylib-webapp/src/main/java/com/citylib/citylibwebapp/service/UserServiceImpl.java
+++ citylib-webapp/src/main/java/com/citylib/citylibwebapp/service/UserServiceImpl.java
@@ -1,19 +1,17 @@
 package com.citylib.citylibwebapp.service;
 
 import com.citylib.citylibwebapp.dto.UserDto;
-import com.citylib.citylibwebapp.model.Role;
-import com.citylib.citylibwebapp.model.User;
+import com.citylib.citylibwebapp.model.RoleBean;
+import com.citylib.citylibwebapp.model.UserBean;
 import com.citylib.citylibwebapp.proxy.CitylibServicesProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -28,7 +26,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = servicesProxy.getUserByEmail(email);
+        UserBean user = servicesProxy.getUserByEmail(email);
         if (user == null) {
             throw new UsernameNotFoundException("Email ou mot de passe invalide.");
         }
@@ -37,19 +35,19 @@ public class UserServiceImpl implements UserService {
             mapRolesToAuthorities(user.getRoles()));
     }
 
-    public User findByEmail(String email) {
+    public UserBean findByEmail(String email) {
         return servicesProxy.getUserByEmail(email);
     }
 
-    public User save(UserDto registration) {
-        User user = new User();
+    public UserBean save(UserDto registration) {
+        UserBean user = new UserBean();
         user.setEmail(registration.getEmail());
         user.setPassword(passwordEncoder.encode(registration.getPassword()));
         user.setUsername(registration.getUsername());
         return servicesProxy.registerUserAccount(user);
     }
 
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<RoleBean> roles) {
         return roles.stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toList());
