8830d4836c0973d6d60524d4a0b42e8d8a777098
CROS Arthur <cros.arthur@gmail.com>
2020-05-20 12:12:22 +0200

    Cleaning up Services' BookRepository

--- citylib-services/src/main/java/com/citylib/citylibservices/repository/BookRepository.java
+++ citylib-services/src/main/java/com/citylib/citylibservices/repository/BookRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByIsbnContainingIgnoreCaseOrTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String isbn, String title, String author, Pageable pageable);
     Page<Book> findAll(Pageable pageable);
-    //TODO Pagination
 }
