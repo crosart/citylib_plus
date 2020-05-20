36dea6cbf8f6a66f7887d59e76c7a8a3bb82e71b
CROS Arthur <cros.arthur@gmail.com>
2020-05-20 10:39:17 +0200

    Updating the 'index' controller
    Changing the List<> retrieved from the Services to Page<> and removing the "Book" Controllers as they are being moved to an independent controller.

--- citylib-webapp/src/main/java/com/citylib/citylibwebapp/controller/WebappController.java
+++ citylib-webapp/src/main/java/com/citylib/citylibwebapp/controller/WebappController.java
@@ -1,12 +1,12 @@
 package com.citylib.citylibwebapp.controller;
 
-import com.citylib.citylibwebapp.model.Book;
+import com.citylib.citylibwebapp.model.BookBean;
 import com.citylib.citylibwebapp.proxy.CitylibServicesProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,28 +18,15 @@ public class WebappController {
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Book> books = servicesProxy.getAllBooks();
+        Page<BookBean> booksPage = servicesProxy.getLastBooks();
+        List<BookBean> books = booksPage.getContent();
         model.addAttribute("books", books);
         return "index";
-    }
-
-    @GetMapping("/books/{id}")
-    public String bookDetail(@PathVariable int id, Model model) {
-        Book book = servicesProxy.getBookById(id);
-        model.addAttribute("book", book);
-        return "bookDetail";
     }
 
     @GetMapping("/login")
     public String login(Model model) {
         return "login";
     }
-
-    @GetMapping("/user")
-    public String userIndex() {
-        return "user/index";
-    }
-
-
 
 }
