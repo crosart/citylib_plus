025ed32bcd9faff006375e8b49dda5a8d8819888
CROS Arthur <cros.arthur@gmail.com>
2020-06-25 10:08:56 +0200

    feat(services): add ReservationDto Object
    Issue #1

--- citylib-services/src/main/java/com/citylib/citylibservices/dto/ReservationDto.java
+++ citylib-services/src/main/java/com/citylib/citylibservices/dto/ReservationDto.java
@@ -2,13 +2,27 @@ package com.citylib.citylibservices.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+/**
+ * DTO reservation object to retrieve data sent by the frontend service and serve it to the controller.
+ *
+ * @author crosart
+ */
 @Data
 public class ReservationDto {
 
+    @NotNull
+    @NotEmpty
     private long id;
 
+    @NotNull
+    @NotEmpty
     private long bookId;
 
+    @NotNull
+    @NotEmpty
     private long userId;
 
 }
