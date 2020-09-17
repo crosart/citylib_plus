bb37f6e67a47b2fb5d171ba92efb36b84dea9bd0
CROS Arthur <cros.arthur@gmail.com>
2020-09-17 10:12:58 +0200

    refactor(services): move asJsonString class to AbstractTest

--- citylib-services/src/test/java/com/citylib/citylibservices/AbstractControllerTest.java
+++ citylib-services/src/test/java/com/citylib/citylibservices/AbstractControllerTest.java
@@ -1,5 +1,6 @@
 package com.citylib.citylibservices;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.TestPropertySource;
@@ -9,4 +10,13 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application.properties")
 public abstract class AbstractControllerTest {
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- citylib-services/src/test/java/com/citylib/citylibservices/UserControllerTest.java
+++ citylib-services/src/test/java/com/citylib/citylibservices/UserControllerTest.java
@@ -44,13 +44,7 @@ public class UserControllerTest extends AbstractControllerTest {
 
     private ObjectBuilder objectBuilder;
 
-    public static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+
 
     @BeforeEach
     void initObjectBuilder() {
